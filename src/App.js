d0078458d33b43340ebd98e52dad39091fc91453
Kenzy Ragab SobhyMohmed <ragabsobhy222@gmail.com>
2026-01-05 17:40:36 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { FaFacebookF, FaInstagram, FaTiktok } from "react-icons/fa";
+import "./App.css";
+import logo from "./logo.png";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="page">
+      <div className="circle-layout">
+
+        {/* Center Image */}
+        <div className="center-image">
+          <img src={logo} alt="My Brand" />
+        </div>
+
+        {/* Social Icons */}
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://web.facebook.com/sinai.oils"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noreferrer"
+          className="icon facebook"
+          style={{ transform: "rotate(-20deg) translate(120px) rotate(20deg)" }}
         >
-          Learn React
+           <div className="float">
+          <FaFacebookF />
+          </div>
         </a>
-      </header>
+
+        <a
+          href="https://www.instagram.com/dahabnaturalroyal?igsh=MWY2NGZoaGoyZXp1eQ%3D%3D"
+          target="_blank"
+          rel="noreferrer"
+          className="icon instagram"
+          style={{ transform: "rotate(105deg) translate(120px) rotate(-105deg)" }}
+        >
+           <div className="float">
+          <FaInstagram />
+          </div>
+        </a>
+
+        <a
+          href="https://www.tiktok.com/@dahab.natural.roy?_r=1&_t=ZS-92kJv9764Gt"
+          target="_blank"
+          rel="noreferrer"
+          className="icon tiktok"
+          style={{ transform: "rotate(195deg) translate(180px) rotate(-195deg)" }}
+        >
+           <div className="float">
+          <FaTiktok />
+          </div>
+        </a>
+
+      </div>
     </div>
   );
 }
